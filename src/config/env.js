082e6c970736d467e26161c0843cb5dbbcd6ff02
shirunpei <shirunpei@chudaokeji.com>
2018-03-20 13:39:21 +0800

    add command

--- src/config/env.js
+++ src/config/env.js
@@ -2,11 +2,9 @@ let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   // 开发环境
   baseURL = 'http://192.168.3.179:3000'
-  // baseURL = 'http://192.168.2.206:3000'
-  // baseURL = 'http://192.168.0.107:3000'
 } else if (process.env.type === 'test') {
   // 测试环境
-  baseURL = 'localhost:9090'
+  baseURL = 'http://172.16.30.128:3000/'
 } else {
   // 正式环境
   baseURL = 'localhost:9090'
